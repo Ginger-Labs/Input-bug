0a0dd5484eebb52b4197c181ebc04f55acffd3c7
garrett <garrett@gingerlabs.com>
2020-06-03 19:47:47 -0700

    Some code to break TI

--- Main.js
+++ Main.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View, TextInput} from 'react-native';
 
-export default class Main extends Component {
+function replace(previous, range, chars) {
+  const text = previous.slice(0, range.start) + chars + previous.slice(range.end)
+  const sel = range.start + chars.length
+  return {
+    text,
+    selection: {start: sel, end: sel}
+  }
+}
 
+export default class Main extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      selection: undefined,
-      text: undefined
-    };
+      selection: null,
+      text: ''
+    }
   }
 
-  onPress() {
+  resetText = () => {
     this.setState({
       selection: {
         start: 10,
@@ -21,16 +29,86 @@ export default class Main extends Component {
     })
   }
 
+  onSelectionChange = evt => {
+    console.log('onSelectionChange: ', evt.nativeEvent)
+
+    let nextSelection = evt.nativeEvent.selection
+    if (!this.inputEvent) {
+      return this.setState({selection: nextSelection})
+    }
+
+    const {previousText, text, range} = this.inputEvent
+    let nextText = replace(this.state.text, range, text).text
+    delete this.inputEvent
+
+    function replaceInputWith(chars) {
+      const result = replace(previousText, range, chars)
+      nextText = result.text
+      nextSelection = result.selection
+    }
+
+    switch (text) {
+      case '@': {
+        replaceInputWith('@Mihail')
+        break
+      }
+
+      case '\n': {
+        replaceInputWith('\n- ')
+        break
+      }
+    }
+
+    this.setState({
+      text: nextText,
+      selection: nextSelection
+    })
+  }
+
+  onChange = evt => {
+    console.log('onChange: ', evt.nativeEvent)
+  }
+
+  onChangeText = evt => {
+    console.log('onChangeText: ', evt)
+  }
+
+  setState(state) {
+    console.log('SetState: ', state)
+    super.setState(state)
+  }
+
+  onTextInput = evt => {
+    console.log('onTextInput: ', evt.nativeEvent)
+    this.inputEvent = evt.nativeEvent
+  }
+
+  onKeyPress = evt => {
+    console.log('onKeyPress: ', evt.nativeEvent)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput selection={this.state.selection} value={this.state.text} placeholder={'Say Something'} />
-        <TouchableOpacity onPress={this.onPress.bind(this)} style={{
+        <TextInput 
+          selection={this.state.selection}
+          value={this.state.text}
+          placeholder={'Say Something'}
+          onSelectionChange={this.onSelectionChange}
+          onChange={this.onChange}
+          onChangeText={this.onChangeText}
+          onTextInput={this.onTextInput}
+          onKeyPress={this.onKeyPress}
+          multiline
+          autoFocus
+        />
+        <TouchableOpacity onPress={this.resetText} style={{
           marginTop: 16
         }} >
           <Text>Click me</Text>
         </TouchableOpacity>
-      </View>);
+      </View>
+    )
   }
 }
 
@@ -50,4 +128,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
